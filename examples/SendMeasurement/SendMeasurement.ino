aaa67a85e93d033292642657e2086bfaddde6e50
Misja Heuveling <misja@heuveling.net>
2020-10-10 01:24:42 +0200

    included wifi example for ESP 8266

--- examples/SendMeasurement/SendMeasurement.ino
+++ examples/SendMeasurement/SendMeasurement.ino
@@ -1,5 +1,9 @@
 #include <CumulocityClient.h>
-#include <WiFi.h>
+#ifdef ESP8266
+ #include <ESP8266WiFi.h>
+#else //ESP32
+ #include <WiFi.h>
+#endif
 
 const char* ssid = "........";
 const char* wifiPassword = "........";
@@ -17,7 +21,7 @@ CumulocityClient c8yClient(pubSubClient, tenant, username, c8yPassword, clientId
 void setup() {
 
   Serial.begin(115200);
-  
+
   WiFi.begin(ssid, wifiPassword);
 
   Serial.print("Connecting to WiFi");
@@ -26,27 +30,27 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("connected to wifi");
-  
+
   c8yClient.connect();
-  
+
   Serial.println("Retrieving device credentials");
-  
+
   c8yClient.retrieveDeviceCredentials();
   while (!c8yClient.checkCredentialsReceived()) {
     Serial.print("#");
     delay(1000);
   }
-  
+
   c8yClient.disconnect();
   c8yClient.connect();
-  
+
   c8yClient.registerDevice("ESP32 - Misja", "c8y_esp32");
-    
+
 }
 
 void loop() {
-  
+
   delay(1000);
   c8yClient.loop();
-  
+
 }
